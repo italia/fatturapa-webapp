fc97f6d74742972d536a8d059e31e2eff5fe2c28
Marco Peca <marco.peca88@gmail.com>
2018-08-02 17:45:10 +0200

    Aggiunti Controller

---
+++ application/controllers/Clienti.php
@@ -0,0 +1,8 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Clienti  extends MY_Controller {
+
+    function index(){        
+    }
+}
---
+++ application/controllers/Profilo.php
@@ -0,0 +1,8 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Profilo  extends MY_Controller {
+
+    function index(){        
+    }
+}
