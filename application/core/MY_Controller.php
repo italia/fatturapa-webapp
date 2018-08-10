a90ce221c68be3cca1cb65da41b6de13e59dcbd2
Marco Peca <marco.peca88@gmail.com>
2018-08-10 18:44:08 +0200

    Aggiunto inclusione css font icon

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -10,10 +10,13 @@ class MY_Controller extends CI_Controller {
         $this->data["base_url"] = base_url();
         $this->data["site_css"] = $this->add_css();
         $this->data["site_js"] = $this->add_js();
+        $this->data["breadcrumb"] = $this->create_breadcrumb();
     }
 
     public function add_css(){
-        $css[] = array("location" => "css/dist/coreui.min.css");        
+        $css[] = array("location" => "css/dist/coreui.min.css");
+        $css[] = array("location" => "css/coreui-icons.min.css");
+        $css[] = array("location" => "css/simple-line-icons.css");
 
         return $css;
     }
@@ -25,6 +28,16 @@ class MY_Controller extends CI_Controller {
         $js[] = array("location" => "js/dist/coreui.min.js");
 
         return $js;
+    }
+
+    public function create_breadcrumb(){        
+        $breadcrumb = array();
+        $crumbs = explode("/",$_SERVER["REQUEST_URI"]);
+        foreach($crumbs as $crumb){             
+             $breadcrumb[] = array("name" => ucfirst(str_replace(array(".php","_"),array(""," "),$crumb) . ' '));
+        } 
+
+        return $breadcrumb;
     }
 
 }
