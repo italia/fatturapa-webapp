858472444b728106f3d329c7720f2a7452fa54a9
Marco Peca <marco.peca88@gmail.com>
2018-08-23 21:41:02 +0200

    Rimosso controller ridondante

--- application/controllers/Start.php
+++
@@ -1,9 +0,0 @@
-<?php 
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class Start extends MY_Controller {
-
-    function index(){
-        $this->smarty->view( 'start.tpl', $this->data );
-    }
-}
